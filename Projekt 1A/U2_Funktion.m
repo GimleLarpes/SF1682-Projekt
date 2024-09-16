f72f3502ba18963f27d0c3f75745039954952939
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-09-16 16:20:48 +0200

    stuff

---
+++ Projekt 1A/U2_Funktion.m
@@ -0,0 +1,15 @@
+function [xbar, ybar, R, Tlap] = robotcircle(b, wL, wR, x0, y0, theta0)
+    % Definierar B, D
+    B = (wR + wL)/2;
+    D = (wR - wL)/2;
+
+    % Mittpunkt
+    xbar = x0 - B/D * sin(theta0);
+    ybar = y0 - B/D * cos(theta0);
+
+    % Radie
+    R = abs(B/D);
+
+    % T_lap
+    Tlap = 2*pi/D;
+end
