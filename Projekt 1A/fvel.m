5fbc718d000635c55f119cf7e9fee5d493ab89ee
Jennifer327 <jennifer.blomstrand@gmail.com>
2024-09-16 16:58:16 +0200

    Update and rename fvel to fvel.m

---
+++ Projekt 1A/fvel.m
@@ -0,0 +1,17 @@
+function ds=fvel(t, s, b, aL, aR, wL, wR)
+
+x = s(1);
+y = s(2);
+theta = s(3);
+vR = aR*t + wR;
+vL = aL*t + wL;
+v = (vR + vL)/2;
+
+dx = v*cos(theta);
+dy = v*sin(theta;
+
+dtheta = (vR - vL)/b;
+
+ds = [dx, dy, dtheta];
+
+end
