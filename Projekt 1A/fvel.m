20dc859550f854fa666cedfbf368726d0fbb5489
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-09-16 17:03:21 +0200

    return of the kommentering

--- Projekt 1A/fvel.m
+++ Projekt 1A/fvel.m
@@ -1,17 +1,18 @@
 function ds=fvel(t, s, b, aL, aR, wL, wR)
+    % Läser x, y , theta från s
+    x = s(1);
+    y = s(2);
+    theta = s(3);
 
-x = s(1);
-y = s(2);
-theta = s(3);
-vR = aR*t + wR;
-vL = aL*t + wL;
-v = (vR + vL)/2;
+    vR = aR*t + wR;
+    vL = aL*t + wL;
+    v = (vR + vL)/2;
 
-dx = v*cos(theta);
-dy = v*sin(theta);
+    % Beräknar dx, dy, dtheta
+    dx = v*cos(theta);
+    dy = v*sin(theta);
 
-dtheta = (vR - vL)/b;
+    dtheta = (vR - vL)/b;
 
-ds = [dx, dy, dtheta];
-
+    ds = [dx, dy, dtheta];
 end
