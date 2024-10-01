f748560ed26ca97657edb9b03f4e0eb5b0efad17
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-10-01 13:06:54 +0200

    improved rk4

--- Projekt 1A/rk4.m
+++ Projekt 1A/rk4.m
@@ -3,9 +3,14 @@ function [tv, yv]=rk4(f, tspan, y, n)
     t = tspan(1);
     h = (tspan(2) - tspan(1)) / n;
 
-    tv=[t];
-    yv=[y'];
+    tv=t;
+    yv=y';
     while t < tspan(2)
+        %Adjust step length to not overshoot
+        if tspan(2) - t < h
+            h = tspan(2) - t + eps;
+        end
+
         s1 = f(t,y);
         s2 = f(t + 0.5*h, y + 0.5*s1*h);
         s3 = f(t + 0.5*h, y + 0.5*s2*h);
