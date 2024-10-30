dd06669d2bf3902b9571e364b74b7345754aebb7
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-10-30 18:16:46 +0100

    added quartercar.m

--- Projekt 1B/quartercar.m
+++ Projekt 1B/quartercar.m
@@ -1,1 +1,10 @@
-function [z1, z2, zprick1, zprick2] = quartercar(m1, m2, k1, k2, c1, c2, v, H, L)
+%% quartercar.m
+function dv=quartercar(v, h, dh, k1, k2, c1, c2, m1, m2)
+    H = [0, 0, 1, 0;
+         0, 0, 0, 1;
+         -k1/m1, k1/m1, -c1/m1, c1/m1;
+         k1/m1, -(k1 + k2) / m2, c1/m2, -(c1 + c2) / m2];
+    g = [0; (c2*dh + k2*h) / m2];
+
+    dv = H*v + g;
+end
