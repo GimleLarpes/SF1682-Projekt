d954ed2b6ee399a8325a25275499b8bea6e01ebf
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-11-06 16:17:37 +0100

    g

--- Projekt 1B/U5.m
+++ Projekt 1B/U5.m
@@ -1,11 +1,53 @@
 %% gör en implementation av den implicita trapetsmetoden (theta = 0.5)
 
+
+
 % a) använd värdena från U4d, kör koden med tidssteget alpha*deltatmax med
 % deltatmax som beräknats för euler framåt, för alpha = 1, 10, 100. vad
 % händer? stämmer det med teorin?
+z1 = 0; z2 = 0; zprick1 = 0; zprick2 = 0;
+m1 = 465; m2 = 55; k1 = 5350; k2 = 13610000; c1 = 310; c2 = 1250; v = 63/3.6; H = 0.27; L = 1.1;
+v_vec0 = [z1; z2; zprick1; zprick2];
+T = 0.5;
+
+h_max = 0.0001;%0.0395; %deltatmax beräknat i U4
+tspan = [0, T];
+
+nZ=[];
+nZT=[];
+for n=0:2
+    h = 10^n * h_max;
+
+    t = 0;
+    v_vec = v_vec0;
+    %Inv trapets
+    [Z,ZT] = qc_inv_trap(tspan, h, v_vec, k1, k2, c1, c2, m1, m2, H, L, v);
+
+    switch n
+        case 0
+            n0Z=Z;
+            n0ZT=ZT;
+        case 1
+            n1Z=Z;
+            n1ZT=ZT;
+        otherwise
+            n2Z=Z;
+            n2ZT=ZT;
+    end
+end
+
+%Plot
+hold on
+plot(n0ZT,n0Z(2,:))
 
 %% b) konvergensstudie fram till t = 0.05 s, k2 = 100*k2,ref
 % normen av felet = max absolutbelopp av felet i z2 i intervallet
+
+%Beräknar exakt lösning
+options = odeset('RelTol',1e-9,'Refine',1);
+
+[Rt,Rv] = ode45(@(t,y) quartercar(t, v_vec, k1, k2, c1, c2, m1, m2, H, L, v),tspan,v_vec0,options);
+%e0 = max(abs(n0Z(2,:)));
 
 % referenslösning med ode45, reltol abstol = 10^-9. ändra tspan för att få
 % lösningen i sökta tidspunkter
---
+++ Projekt 1B/qc_inv_trap.m
@@ -0,0 +1,40 @@
+function [vl,tl]=qc_inv_trap(tspan, h, v_vec, k1, k2, c1, c2, m1, m2, H, L, v)
+    
+    H_matrix = [0, 0, 1, 0;
+                0, 0, 0, 1;
+               -k1 / m1, k1 / m1, -c1 / m1, c1 / m1;
+                k1 / m1, -((k1 + k2) / m2), c1 / m2, -((c1 + c2) / m2)];
+
+    vl=[];
+    tl=[];
+    dt=h;
+    t=tspan(1);
+    while t<tspan(2)-h
+        %Adjust step length to not overshoot
+        if tspan(2) - t < dt
+            dt = tspan(2) - t + eps;
+        end
+        
+        %Calculate stuff
+        g = calc_g(t, k2, c2, m2, H, L, v);
+
+        dv = H_matrix*v_vec + g;
+
+        v_vec = v_vec + 0.5*dt * (H_matrix*v_vec + H_matrix*(v_vec + dv) + g + calc_g(t+dt, k2, c2, m2, H, L, v));
+        
+        vl = cat(2, vl, v_vec);
+        tl = cat(2, tl, t);
+        t=t+dt;
+    end
+end
+
+function g=calc_g(t, k2, c2, m2, H, L, v)
+    if t <= L/v
+        h = H/2*(1-cos((2*pi*v*t)/L));
+        dh = ((2*pi*v)/L)*sin((2*pi*v*t)/L);
+    else
+        h = 0;
+        dh = 0;
+    end
+    g = [0; 0; 0; (c2*dh + k2*h) / m2];
+end
