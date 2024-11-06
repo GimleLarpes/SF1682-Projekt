e34480b54c2f13ad3272cb722bc6fa524029279c
Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>
2024-11-06 17:28:29 +0100

    k

--- Projekt 1B/U5.m
+++ Projekt 1B/U5.m
@@ -10,7 +10,7 @@ m1 = 465; m2 = 55; k1 = 5350; k2 = 13610000; c1 = 310; c2 = 1250; v = 63/3.6; H 
 v_vec0 = [z1; z2; zprick1; zprick2];
 T = 0.05;
 
-h_max = 0.0001;%0.0395; %deltatmax beräknat i U4
+h_max = 0.001;%0.0395; %deltatmax beräknat i U4
 tspan = [0, T];
 
 nZ=[];
@@ -19,9 +19,8 @@ for n=0:2
     h = 10^n * h_max;
 
     t = 0;
-    v_vec = v_vec0;
     %Inv trapets
-    [Z,ZT] = qc_inv_trap(tspan, h, v_vec, k1, k2, c1, c2, m1, m2, H, L, v);
+    [Z,ZT] = qc_inv_trap(tspan, h, v_vec0, k1, k2, c1, c2, m1, m2, H, L, v);
 
     switch n
         case 0
@@ -44,11 +43,23 @@ plot(n0ZT,n0Z(2,:))
 % normen av felet = max absolutbelopp av felet i z2 i intervallet
 
 %Beräknar exakt lösning
-%tspan=[0.1,0.15];
+tspan=[0,0.05];
+dt0 = 0.01;
 options = odeset('RelTol',1e-9,'Refine',1);
 
 [Rt,Rv] = ode45(@(t,y) quartercar(t, v_vec, k1, k2, c1, c2, m1, m2, H, L, v),tspan,v_vec0,options);
-%e0 = max(abs(n0Z(2,:)));
+
+%Olika dt
+[e0Z,e0ZT] = qc_inv_trap(tspan, dt0, v_vec0, k1, k2, c1, c2, m1, m2, H, L, v);
+[e1Z,e1ZT] = qc_inv_trap(tspan, dt0/2, v_vec0, k1, k2, c1, c2, m1, m2, H, L, v);
+[e2Z,e2ZT] = qc_inv_trap(tspan, dt0/4, v_vec0, k1, k2, c1, c2, m1, m2, H, L, v);
+[e3Z,e3ZT] = qc_inv_trap(tspan, dt0/8, v_vec0, k1, k2, c1, c2, m1, m2, H, L, v);
+
+%Beräknar fel
+e0 = max(abs(e0Z(2,end)-Rv(2,end)))
+e1 = max(abs(e1Z(2,end)-Rv(2,end)))
+e2 = max(abs(e2Z(2,end)-Rv(2,end)))
+e3 = max(abs(e3Z(2,end)-Rv(2,end)))
 
 % referenslösning med ode45, reltol abstol = 10^-9. ändra tspan för att få
 % lösningen i sökta tidspunkter
